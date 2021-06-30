6eedba1c7b79d26c8e55da1f349fbdc1635202c7
Sendii <sendidianhadiwijaya@gmail.com>
2021-06-30 14:35:35 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,39 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Ini title</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col">
+                <div class="card">
+                    <form action="{{url('save')}}" method="POST" enctype="multipart/form-data">
+                        <div class="card-body shadow">
+                            @csrf
+                            <div class="form-group">
+                                <label for="">Nama</label>
+                                <input type="text" class="form-control" name="name" placeholder="Masukkan name" value="sendi"> <br>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Masukkan email" value="sendi@sendi.com {{rand(1,100)}}"> <br>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="password" placeholder="Masukkan password" value="111111"> <br>
+                            </div>
+                            <div class="form-group">
+                                <!-- multiple file -->
+                                <input type="file" class="form-control" name="file_photo[]" multiple placeholder="Masukkan poto"> <br> 
+                                <!-- single file -->
+                                <!-- <input type="file" name="file_photo" placeholder="Masukkan poto"> <br> -->
+                            </div>
+                            <button class="btn btn-primary" type="submit">Simpan</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('delete', function(){
 Route::post('/save', function (Request $req) {
     $a = new Customize(\App\User::class);
     // return $a->storeData($req->all(), true);
-    return $a->updateData(2, $req->all(), true);
+    return $a->updateData(7, $req->all(), true);
     // $req['_token'] = 'ERi7HVxwlcOcj8gY4kSvT3fuqRwJC2R9L3ICbL4q';
     // $req['name'] = 'sendi';
     // $req['email'] = 'sendi@sendi.cos'.rand(1, 1000);
@@ -33,3 +33,4 @@ Route::post('/save', function (Request $req) {
     
 });
 Route::view('create', 'create');
+Route::view('/', 'welcome');
